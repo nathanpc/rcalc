6fad23119759a92670677422a8acbd9b5d85ed07
Nathan Campos <eeepc904@gmail.com>
2013-05-07 12:27:11 -0300

    Made a little refactor

--- js/resistance.js
+++ js/resistance.js
@@ -4,65 +4,65 @@
 var resistance = {};
 
 resistance.values = {
-	"black": {
-		"band": 0,
-		"multiplier": 1,
-		"tolerance": null
+	black: {
+		band: 0,
+		multiplier: 1,
+		tolerance: null
 	},
-	"brown": {
-		"band": 1,
-		"multiplier": 10,
-		"tolerance": 1
+	brown: {
+		band: 1,
+		multiplier: 10,
+		tolerance: 1
 	},
-	"red": {
-		"band": 2,
-		"multiplier": 100,
-		"tolerance": 2
+	red: {
+		band: 2,
+		multiplier: 100,
+		tolerance: 2
 	},
-	"orange": {
-		"band": 3,
-		"multiplier": 1000,
-		"tolerance": null
+	orange: {
+		band: 3,
+		multiplier: 1000,
+		tolerance: null
 	},
-	"yellow": {
-		"band": 4,
-		"multiplier": 10000,
-		"tolerance": null
+	yellow: {
+		band: 4,
+		multiplier: 10000,
+		tolerance: null
 	},
-	"green": {
-		"band": 5,
-		"multiplier": 100000,
-		"tolerance": 0.5
+	green: {
+		band: 5,
+		multiplier: 100000,
+		tolerance: 0.5
 	},
-	"blue": {
-		"band": 6,
-		"multiplier": 1000000,
-		"tolerance": 0.25
+	blue: {
+		band: 6,
+		multiplier: 1000000,
+		tolerance: 0.25
 	},
-	"violet": {
-		"band": 7,
-		"multiplier": 10000000,
-		"tolerance": 0.1
+	violet: {
+		band: 7,
+		multiplier: 10000000,
+		tolerance: 0.1
 	},
-	"grey": {
-		"band": 8,
-		"multiplier": null,
-		"tolerance": 0.05
+	gray: {
+		band: 8,
+		multiplier: null,
+		tolerance: 0.05
 	},
-	"white": {
-		"band": 9,
-		"multiplier": null,
-		"tolerance": null
+	white: {
+		band: 9,
+		multiplier: null,
+		tolerance: null
 	},
-	"gold": {
-		"band": null,
-		"multiplier": 0.1,
-		"tolerance": 5
+	gold: {
+		band: null,
+		multiplier: 0.1,
+		tolerance: 5
 	},
-	"silver": {
-		"band": null,
-		"multiplier": 0.01,
-		"tolerance": 10
+	silver: {
+		band: null,
+		multiplier: 0.01,
+		tolerance: 10
 	}
 };
 
@@ -73,14 +73,23 @@ resistance.values = {
  * @return {Number} Resistance
  */
 resistance.calc = function (bands) {
-	var resistance = 0;
+	var res = "";
 
 	for (var i = 0; i < bands.length; i++) {
-		//
-	}
-	// TODO: Calculate?
+		var band = bands[i];
 
-	return resistance;
+		// Check if it's the last band (multiplier).
+		if (bands[i + 1] === undefined) {
+			// Convert to Number and apply the multiplier.
+			res = parseInt(res, 10);
+			res *= resistance.values[band].multiplier;
+		} else {
+			// Append the value to the string.
+			res += String(resistance.values[band].band);
+		}
+	}
+
+	return res;
 }
 
 /**
@@ -90,9 +99,10 @@ resistance.calc = function (bands) {
  * @param {Boolean} use_symbol Use ohms symbol?
  * @return {String} Pretty-printed value.
  */
-resistance.pretty_print = function (value, use_symbol) {
+resistance.pretty_print = function (value, tolerance, use_symbol) {
 	var result = "";
 
+	// Value.
 	if (value >= 1000000) {
 		result = (value / 1000000) + "M";
 	} else if (value >= 1000) {
@@ -101,10 +111,16 @@ resistance.pretty_print = function (value, use_symbol) {
 		result += value;
 	}
 
+	// Ohms symbol.
 	if (use_symbol || use_symbol === undefined) {
 		result += "\u2126";
 	} else {
 		result += " ohms";
+	}
+
+	// Tolenrance.
+	if (tolerance !== undefined) {
+		result +=  "\u00B1" + resistance.values[tolerance].tolerance + "%";
 	}
 
 	return result;
